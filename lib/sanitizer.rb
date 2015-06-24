5b7d5f57d36a9cc139d2d221da5adb1b190a9cc0
Jamie Wright <jamie@brilliantfantastic.com>
2015-06-24 14:09:12 -0400

    Sanitizer sanitize and unsanitizer methods

--- lib/sanitizer.rb
+++ lib/sanitizer.rb
@@ -1,8 +1,21 @@
 class Sanitizer
   attr_reader :sanitized_text, :text
+  attr_accessor :rules
 
   def initialize(text)
     @text = text
     @sanitized_text = text
+    @rules = []
+  end
+
+  def sanitize(*rules)
+    self.rules = *rules
+    self.rules.map { |rule| @sanitized_text = rule.sanitize(sanitized_text) }
+    self
+  end
+
+  def unsanitize
+    self.rules.reverse.map { |rule| @sanitized_text = rule.unsanitize(sanitized_text) }
+    self
   end
 end
--- spec/sanitizer_spec.rb
+++ spec/sanitizer_spec.rb
@@ -7,4 +7,23 @@ describe Sanitizer do
   it "returns the same text if it has not been sanitized" do
     expect(subject.sanitized_text).to eq text
   end
+
+  describe "#sanitize" do
+    let(:rule) { double(:sanitation_rule, sanitize: text) }
+
+    it "runs the sanitation rules that are specified" do
+      expect(rule).to receive(:sanitize).with(text).and_return(subject)
+      subject.sanitize(rule)
+    end
+  end
+
+  describe "#unsanitize" do
+    let(:rule) { double(:sanitation_rule, sanitize: text, unsanitize: text) }
+
+    it "runs the same sanitation rules when the text was sanitized" do
+      subject.sanitize(rule)
+      expect(rule).to receive(:unsanitize).with(text).and_return(subject)
+      subject.unsanitize
+    end
+  end
 end

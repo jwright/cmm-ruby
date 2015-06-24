96e27e5c007a68d3f8d19ae5e92cd677f832d485
Jamie Wright <jamie@brilliantfantastic.com>
2015-06-24 14:44:10 -0400

    Use the time period sanitation rule

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -1,6 +1,7 @@
 require_relative "sanitizer"
 require_relative "contraction_sanitizer_rule"
 require_relative "time_sanitizer_rule"
+require_relative "time_period_sanitizer_rule"
 
 class WordCounter
   attr_reader :results, :sanitizer, :text
@@ -12,14 +13,14 @@ class WordCounter
   end
 
   def count_words
-    sanitized_text = sanitizer.sanitize(ContractionSanitizerRule.new, TimeSanitizerRule.new).sanitized_text
-    sanitized_text = sanitize_time_period(sanitized_text)
+    sanitized_text = sanitizer.sanitize(ContractionSanitizerRule.new,
+                                        TimeSanitizerRule.new,
+                                        TimePeriodSanitizerRule.new).sanitized_text
 
     words = sanitized_text.scan(/\b(\w+)\b/).flatten
 
     words.to_a.each do |word|
       insensitive_word = sanitizer.unsanitize(word.downcase).sanitized_text
-      insensitive_word = unsanitize_time_period insensitive_word
 
       if results.key?(insensitive_word)
         results[insensitive_word] += 1
@@ -29,13 +30,5 @@ class WordCounter
     end
 
     results
-  end
-
-  def sanitize_time_period(words)
-    words.gsub(/[p|P].[m|M]./, "__pm__").gsub(/[a|A].[m|M]./, "__am__")
-  end
-
-  def unsanitize_time_period(words)
-    words.gsub(/__pm__/, "p.m.").gsub(/__am__/, "a.m.")
   end
 end
